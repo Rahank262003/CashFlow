e8847e2ef94ac9999ff06caaf8ed6e6b9a9d9ef3
Rahil <rahil.ankhad@spit.ac.in>
2023-11-23 19:13:19 +0530

    middleware

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles.middleware.StaticFilesMiddleware'
 ]
 
 ROOT_URLCONF = 'expenseswebsite.urls'
